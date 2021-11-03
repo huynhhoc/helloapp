06e68a931276f312320ba9a75201ea3c6c7a1413
huynhhoc <huynhhoc@gmail.com>
2021-11-03 12:03:16 +0100

    update

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 # Entry point for the application.
-from . import app    # For application discovery by the 'flask' command.
-from . import views  # For import side-effects of setting up routes.
+#from . import app    # For application discovery by the 'flask' command.
+#from . import views  # For import side-effects of setting up routes.
+import flask
+app = flask.Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(port=8080)
