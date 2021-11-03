6ef5e5939a1b2a050a0d9125b879fafd49644db4
huynhhoc <huynhhoc@gmail.com>
2021-11-03 12:54:42 +0100

    change app to huynhapp

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+huynhapp = Flask(__name__)
 # Replace the existing home function with the one below
-@app.route("/")
+@huynhapp.route("/")
 def home():
     return render_template("home.html")
 
 # New functions
-@app.route("/about/")
+@huynhapp.route("/about/")
 def about():
     return render_template("about.html")
 
-@app.route("/contact/")
+@huynhapp.route("/contact/")
 def contact():
     return render_template("contact.html")
 
 if __name__ == "__main__":
-     app.run()
+     huynhapp.run()
