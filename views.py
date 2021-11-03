4bc131fc07437ed40260b8a57b05f62e3f45c8cc
huynhhoc <huynhhoc@gmail.com>
2021-11-03 11:36:44 +0100

    Procfile

--- views.py
+++ views.py
@@ -18,36 +18,3 @@ def about():
 @app.route("/contact/")
 def contact():
     return render_template("contact.html")
-
-@app.route("/api/data")
-def get_data():
-    return app.send_static_file("data.json")
-
-@app.route("/hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
-
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return content
-@app.route("/bonjour/<name>")
-def bonjour_there(name = None):
-    match_object = re.match("[a-zA-Z]+", name)
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-    return render_template(
-    "hello_there.html",
-    name=clean_name,
-    date=datetime.now()
-    )
