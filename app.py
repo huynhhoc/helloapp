368f6780d5fa55a05c33f40eab983166e189e37a
huynhhoc <huynhhoc@gmail.com>
2021-11-03 12:49:11 +0100

    remove views.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+# Replace the existing home function with the one below
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+# New functions
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
      app.run()
